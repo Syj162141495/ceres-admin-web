c102efb09db55a72d1f3a53b0d5977b254a79738
inkfish-maker <2958506074@qq.com>
2024-05-30 13:35:56 +0800

    订单基础完成

--- src/api/order.js
+++ src/api/order.js
@@ -1,6 +1,7 @@
 import request from '@/utils/request'
 
 //* ******************** 待处理订单  *********************
+
 // 订单管理查询
 export function orderGetAll(data) {
   return request({
@@ -17,20 +18,18 @@ export function orderGetById(data) {
     data
   })
 }
-
-// 选择快递公司查询
-export function orderGetSelect(data) {
-  return request({
-    url: '/express/getExpressSelect',
-    method: 'post',
-    data
-  })
-}
-
 // 发货
 export function orderDilevery(data) {
   return request({
     url: '/order/dilevery',
+    method: 'post',
+    data
+  })
+}
+// 选择快递公司查询
+export function orderGetSelect(data) {
+  return request({
+    url: '/express/getExpressSelect',
     method: 'post',
     data
   })
